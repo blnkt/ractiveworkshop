c42341aa06cb6d2e6d3192a65f22c057d79c18c7
Blake Mason <student@epicodus.com>
2014-09-20 16:09:16 -0700

    um

---
+++ assets/data/dadata.js
@@ -0,0 +1,356 @@
+var dadata = [
+  {
+    "_id": "541dd3c46613b9d4e2f6cdd8",
+    "index": 0,
+    "guid": "b2d84aee-7859-4f28-a326-1164d1e93772",
+    "isActive": true,
+    "balance": "$1,029.21",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "blue",
+    "name": {
+      "first": "Gena",
+      "last": "Wilson"
+    },
+    "company": "OLUCORE",
+    "email": "gena.wilson@olucore.info",
+    "phone": "+1 (968) 574-3145",
+    "address": "228 Cameron Court, Frank, Tennessee, 6701",
+    "about": "Aliquip amet cupidatat reprehenderit incididunt ut ipsum. Elit nulla cupidatat laborum nisi velit ex cillum. Commodo enim est cupidatat ut adipisicing minim id. Nulla aliquip duis aliqua aute et deserunt sunt nisi. Aliquip ad nisi ipsum magna quis commodo cillum do sint minim magna occaecat nulla minim.\r\n",
+    "registered": "Thursday, March 13, 2014 12:32 AM",
+    "latitude": -52.997341,
+    "longitude": 19.371682,
+    "tags": [
+      "dolore",
+      "culpa",
+      "pariatur",
+      "mollit",
+      "laboris",
+      "nisi",
+      "est"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Alicia Vance"
+      },
+      {
+        "id": 1,
+        "name": "Leila Andrews"
+      },
+      {
+        "id": 2,
+        "name": "Mildred Shaw"
+      }
+    ],
+    "greeting": "Hello, Gena! You have 6 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "541dd3c497a333efc3518bf9",
+    "index": 1,
+    "guid": "03edb9e9-0803-4b67-9a09-eb15ee5de643",
+    "isActive": true,
+    "balance": "$3,890.78",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "green",
+    "name": {
+      "first": "Mollie",
+      "last": "Bauer"
+    },
+    "company": "STROZEN",
+    "email": "mollie.bauer@strozen.tv",
+    "phone": "+1 (845) 551-2767",
+    "address": "575 Temple Court, Hayden, Virginia, 5893",
+    "about": "Cillum fugiat velit consectetur mollit id aliquip do Lorem ad labore. Aliquip irure enim ipsum laborum consequat eiusmod laborum nostrud. Dolore mollit non esse pariatur sunt occaecat dolor incididunt ea amet. Magna aliquip dolor eu commodo do exercitation aliquip veniam reprehenderit aute anim et id esse.\r\n",
+    "registered": "Saturday, September 13, 2014 6:19 AM",
+    "latitude": 6.431043,
+    "longitude": 78.725044,
+    "tags": [
+      "amet",
+      "esse",
+      "ut",
+      "ut",
+      "magna",
+      "qui",
+      "ullamco"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Holden Leon"
+      },
+      {
+        "id": 1,
+        "name": "Krystal Phillips"
+      },
+      {
+        "id": 2,
+        "name": "Briggs Ferrell"
+      }
+    ],
+    "greeting": "Hello, Mollie! You have 6 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "541dd3c4ca03d2da59e49976",
+    "index": 2,
+    "guid": "3378dc38-f563-4e32-8c31-2e1285b221e6",
+    "isActive": false,
+    "balance": "$2,830.89",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Sybil",
+      "last": "Gilmore"
+    },
+    "company": "ZANILLA",
+    "email": "sybil.gilmore@zanilla.us",
+    "phone": "+1 (842) 459-2720",
+    "address": "173 Hunts Lane, Succasunna, American Samoa, 2179",
+    "about": "Deserunt ut cillum enim ea. Sit proident officia deserunt cillum Lorem commodo cupidatat excepteur dolore sunt eu ipsum deserunt. Magna labore nisi velit ex eiusmod pariatur cupidatat consectetur ad ex ut. Quis Lorem dolor irure excepteur qui duis aliquip. Deserunt ut proident aliquip anim. In dolor exercitation voluptate labore ex excepteur est officia. Cupidatat cillum veniam eu exercitation occaecat.\r\n",
+    "registered": "Thursday, May 29, 2014 1:19 AM",
+    "latitude": 44.066473,
+    "longitude": 97.378362,
+    "tags": [
+      "incididunt",
+      "cillum",
+      "eu",
+      "dolore",
+      "tempor",
+      "minim",
+      "officia"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Callahan Malone"
+      },
+      {
+        "id": 1,
+        "name": "Roberts Curry"
+      },
+      {
+        "id": 2,
+        "name": "Deloris Gomez"
+      }
+    ],
+    "greeting": "Hello, Sybil! You have 10 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "541dd3c40bfb01bc73d46a1f",
+    "index": 3,
+    "guid": "f3814d98-5ae9-4d97-8b15-ffd3637d9690",
+    "isActive": true,
+    "balance": "$2,615.83",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Powers",
+      "last": "Camacho"
+    },
+    "company": "CENTICE",
+    "email": "powers.camacho@centice.me",
+    "phone": "+1 (803) 512-2349",
+    "address": "787 Eaton Court, Stonybrook, Kentucky, 3826",
+    "about": "Velit eiusmod non adipisicing tempor et. Eu veniam id quis sit eiusmod non qui. Et et qui pariatur fugiat elit sunt ex commodo consequat qui quis pariatur ullamco. Non laborum nostrud et sit. Amet elit id cupidatat enim sit pariatur.\r\n",
+    "registered": "Thursday, August 21, 2014 6:48 PM",
+    "latitude": 24.549174,
+    "longitude": -11.369633,
+    "tags": [
+      "labore",
+      "magna",
+      "incididunt",
+      "exercitation",
+      "elit",
+      "nostrud",
+      "eiusmod"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Stuart Chavez"
+      },
+      {
+        "id": 1,
+        "name": "Penny Dale"
+      },
+      {
+        "id": 2,
+        "name": "Selena Pacheco"
+      }
+    ],
+    "greeting": "Hello, Powers! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "541dd3c4a0b71908badf3eac",
+    "index": 4,
+    "guid": "dd6e42c4-913d-4243-8e46-b3269b3718b6",
+    "isActive": true,
+    "balance": "$2,300.57",
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "green",
+    "name": {
+      "first": "Toni",
+      "last": "Calderon"
+    },
+    "company": "TWIGGERY",
+    "email": "toni.calderon@twiggery.name",
+    "phone": "+1 (815) 596-2840",
+    "address": "874 Schenectady Avenue, Orason, Oklahoma, 3818",
+    "about": "Qui ex commodo eiusmod officia nostrud dolore voluptate dolor eu sunt cupidatat nulla amet. Voluptate nisi ex minim exercitation esse ex ipsum tempor. Ea ut enim est ea tempor aliqua nostrud incididunt. Voluptate do ad nostrud tempor ea dolor esse labore incididunt velit. Duis culpa culpa minim aute magna reprehenderit eu fugiat fugiat enim labore. Ea exercitation occaecat esse ut. Enim excepteur deserunt laboris dolor.\r\n",
+    "registered": "Tuesday, March 4, 2014 9:17 AM",
+    "latitude": -71.18721,
+    "longitude": -142.765426,
+    "tags": [
+      "dolore",
+      "non",
+      "deserunt",
+      "labore",
+      "nulla",
+      "excepteur",
+      "est"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Mcgowan Mann"
+      },
+      {
+        "id": 1,
+        "name": "Dawson Downs"
+      },
+      {
+        "id": 2,
+        "name": "Ana Ramos"
+      }
+    ],
+    "greeting": "Hello, Toni! You have 9 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "541dd3c4c0138c94d003c004",
+    "index": 5,
+    "guid": "7f439e3c-2f99-41bf-b344-57e41e1c3a2d",
+    "isActive": true,
+    "balance": "$1,457.98",
+    "picture": "http://placehold.it/32x32",
+    "age": 39,
+    "eyeColor": "green",
+    "name": {
+      "first": "Heather",
+      "last": "Lang"
+    },
+    "company": "OULU",
+    "email": "heather.lang@oulu.co.uk",
+    "phone": "+1 (975) 441-3045",
+    "address": "725 Imlay Street, Bannock, Montana, 1743",
+    "about": "Minim commodo ad eu ad aute qui aute laboris aliqua esse sunt aute sit. Anim commodo reprehenderit incididunt non incididunt. Et nostrud id anim Lorem aliquip.\r\n",
+    "registered": "Saturday, April 26, 2014 7:37 PM",
+    "latitude": 25.038254,
+    "longitude": -63.548066,
+    "tags": [
+      "do",
+      "occaecat",
+      "eu",
+      "sit",
+      "culpa",
+      "qui",
+      "consectetur"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Boyd Young"
+      },
+      {
+        "id": 1,
+        "name": "Church Preston"
+      },
+      {
+        "id": 2,
+        "name": "Bethany Joyce"
+      }
+    ],
+    "greeting": "Hello, Heather! You have 6 unread messages.",
+    "favoriteFruit": "strawberry"
+  }
+]
